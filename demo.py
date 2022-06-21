4a82dafc963536242e6eea8d350e8c91e5ec74f4
Jake Bringetto <jakebringetto@gmail.com>
2022-06-20 23:18:08 -0700

    initial commit

---
+++ demo.py
@@ -0,0 +1,55 @@
+# Shows a user's playlists (need to be authenticated via oauth)
+
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
+
+def show_tracks(results):
+    curr_tracklist = []
+    for i, item in enumerate(results['items']):
+        track = item['track']
+        curr_tracklist.append(track['id'])
+    return curr_tracklist
+        
+
+
+if __name__ == '__main__':
+    scope = 'playlist-modify-public'
+    sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope))
+
+    all_track_ids = []
+
+    playlists = sp.current_user_playlists()
+    user_id = sp.me()['id']
+
+    everything_playlist = [playlist for playlist in playlists['items'] if playlist['name'] == 'Everything'][0]
+    results = sp.playlist(everything_playlist['id'], fields="tracks,next")
+    tracks = results['tracks']
+
+    tracks_to_exclude = []
+    tracks_to_exclude.extend(show_tracks(tracks))
+    while tracks['next']:
+        tracks = sp.next(tracks)
+        tracks_to_exclude.extend(show_tracks(tracks))
+
+    all_track_ids.extend(show_tracks(tracks))
+    while tracks['next']:
+        tracks = sp.next(tracks)
+        all_track_ids.extend(show_tracks(tracks))
+
+
+    for playlist in playlists['items']:
+        if playlist['name'] != 'Everything':
+            if playlist['owner']['id'] == user_id:
+                results = sp.playlist(playlist['id'], fields="tracks,next")
+                tracks = results['tracks']
+
+                all_track_ids.extend(show_tracks(tracks))
+                while tracks['next']:
+                    tracks = sp.next(tracks)
+                    all_track_ids.extend(show_tracks(tracks))
+
+    all_track_ids = list(set(all_track_ids).difference(set(tracks_to_exclude)))
+    sp.playlist_add_items(everything_playlist['id'], all_track_ids)
+
+
+
